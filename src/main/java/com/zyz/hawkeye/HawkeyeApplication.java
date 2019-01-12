b0dd589b57ae2cab9749035f399d73771a52e2b8
zhuyunzhuang <zhuyunzhuang@u51.com>
2019-01-12 16:12:30 +0800

    Initial commit

---
+++ src/main/java/com/zyz/hawkeye/HawkeyeApplication.java
@@ -0,0 +1,14 @@
+package com.zyz.hawkeye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawkeyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HawkeyeApplication.class, args);
+    }
+
+}
+
